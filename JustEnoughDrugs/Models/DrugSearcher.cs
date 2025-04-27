896a387d316bfe5f0fbf91030fb037aea5132d18
BrandSEPI <brandon.cartier@epitech.eu>
2025-04-27 19:09:28 +0200

    working filter key

--- JustEnoughDrugs/Models/DrugSearcher.cs
+++ JustEnoughDrugs/Models/DrugSearcher.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using MelonLoader;
 using ScheduleOne.Product;
 
 namespace JustEnoughDrugs.Models
@@ -16,64 +17,75 @@ namespace JustEnoughDrugs.Models
             bool matchesName = false;
             bool matchesIngredient = false;
 
-            if (filterKey == "Effects" || filterKey == "Any")
+            if (filterKey == SearchFilter.Effects.ToString() || filterKey == SearchFilter.Any.ToString())
             {
                 matchesEffect = MatchesEffects(productEntry, searchText);
             }
 
-            if (filterKey == "Ingredients" || filterKey == "Any")
+            if (filterKey == SearchFilter.Ingredients.ToString() || filterKey == SearchFilter.Any.ToString())
             {
                 matchesIngredient = MatchesIngredients(productEntry, searchText);
             }
 
-            if (filterKey == "Name" || filterKey == "Any")
+            if (filterKey == SearchFilter.Name.ToString() || filterKey == SearchFilter.Any.ToString())
             {
                 matchesName = MatchesName(productEntry, searchText);
             }
 
             return matchesName || matchesEffect || matchesIngredient;
         }
-
         private bool MatchesName(ProductEntry entry, string searchText)
         {
-            string drugName = entry.Definition.ToString();
-            return MatchesSearch(drugName, searchText);
+            string drugName = entry.Definition.ToString().ToLowerInvariant();
+            return MatchesSearch(new List<string> { drugName }, searchText);
         }
+
 
         private bool MatchesEffects(ProductEntry entry, string searchText)
         {
             var effectList = new List<string>();
-            entry.Definition.Properties.ForEach(p => effectList.Add(p.ToString()));
-            return effectList.Any(effect => MatchesSearch(effect, searchText));
+            entry.Definition.Properties.ForEach(p => effectList.Add(p.ToString().ToLowerInvariant()));
+            return MatchesSearch(effectList, searchText);
         }
 
         private bool MatchesIngredients(ProductEntry entry, string searchText)
         {
             var ingredientList = new List<string>();
             entry.Definition.Recipes.ForEach(r => r.Ingredients.ForEach(i =>
-                ingredientList.Add(i.Item.ToString())));
+                ingredientList.Add(i.Item.ToString().ToLowerInvariant())));
 
-            return ingredientList.Any(ingredient => MatchesSearch(ingredient, searchText));
+            return MatchesSearch(ingredientList, searchText);
         }
-
-        private bool MatchesSearch(string target, string searchText)
+        private bool MatchesSearch(List<string> targetList, string searchText)
         {
-            target = target.ToLowerInvariant();
+            var AndChar = ",";
+            var OrChar = "|";
             searchText = searchText.ToLowerInvariant();
 
-            if (searchText.Contains(","))
+            if (searchText.Contains(AndChar) && searchText.Split(AndChar.ToCharArray())[1] != "")
             {
-                var terms = searchText.Split(',');
-                return terms.All(term => target.Contains(term.Trim()));
+                var terms = searchText.Split(AndChar.ToCharArray());
+                return terms.All(term =>
+                {
+                    return targetList.Any(target => target.Contains(term.Trim()));
+
+                });
             }
-            else if (searchText.Contains("|"))
+            else if (searchText.Contains(OrChar) && searchText.Split(OrChar.ToCharArray())[1] != "")
             {
-                var terms = searchText.Split('|');
-                return terms.Any(term => target.Contains(term.Trim()));
+
+                var terms = searchText.Split(OrChar.ToCharArray());
+
+                return terms.Any(term =>
+                 {
+                     return targetList.Any(target => target.Contains(term.Trim()));
+
+                 });
             }
             else
             {
-                return target.Contains(searchText);
+                return targetList.Any(target => target.Contains(searchText.Replace(OrChar, "").Replace(AndChar, "").Trim()));
+
             }
         }
     }
